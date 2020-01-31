00517b87b8abedc2117d898ef52f4d0880737ea5
NicoFiraDev <nicofira.dev@gmail.com>
2020-01-31 11:13:59 -0500

    Updates

---
+++ precache-manifest.98ebbe24f724073d7236cfa37b5060dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9971f824e097f656fb64fdf1af5619b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "762b343e305d596a0246",
+    "url": "/monsters-rolodex/static/css/main.17334ec1.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "762b343e305d596a0246",
+    "url": "/monsters-rolodex/static/js/main.2daa711e.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
